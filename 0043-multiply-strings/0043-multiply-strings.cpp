a7d23e5266eb6d194760d12b65029a88e6c75bc1
Krihit <mohitppatil2005@gmail.com>
2026-03-27 23:34:04 +0530

    Time: 0 ms (100%), Space: 10.8 MB (18.01%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -3,27 +3,28 @@ public:
     string multiply(string num1, string num2) {
         if(num1 == "0" || num2 == "0") return "0";
 
-        int n1 = num1.size();
-        int n2 = num2.size();
-        vector<int> res(n1 + n2, 0);
-
-        for(int i = n1 - 1; i >= 0; i--){
-            for(int j = n2 - 1; j >= 0; j--){
-                int mul = (num1[i] - '0') * (num2[j] - '0');
-                int sum = mul + res[i + j + 1];
-
-                res[i + j + 1] = sum % 10;
-                res[i + j] += sum / 10;
+        int n1=num1.size();
+        int n2=num2.size();
+        vector<int> ans(n1+n2,0);
+        int space=0;
+        for(int i=n1-1;i>=0;i--){
+            int carry=0;
+            for(int j=n2-1;j>=0;j--){
+                int d1=num1[i]-'0';
+                int d2=num2[j]-'0';
+                int temp=d1*d2+carry+ans[i+j+1];
+                ans[i+j+1]=temp%10;
+                carry=temp/10;
             }
+            ans[i]+=carry;
+            space++;
         }
-
-        string ans = "";
-        for(int x : res){
-            if(!(ans.empty() && x == 0)){
-                ans.push_back(x + '0');
-            }
+        string res="";
+        int i=0;
+        while(i<num1.size() && ans[i]==0) i++;
+        for(;i<(n1+n2);i++){
+            res+=to_string(ans[i]);
         }
-
-        return ans;
+        return res;
     }
 };
