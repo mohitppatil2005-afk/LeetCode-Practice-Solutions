4e62064559da9a5d17093f431e808c64c706daf0
Krihit <mohitppatil2005@gmail.com>
2026-04-01 23:32:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (87.97%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s[0]=='0') return 0;
+        int prev2=1; 
+        int prev1=1; 
+        for(int i=1;i<s.size();i++) {
+            int curr=0;
+            if(s[i]!='0')
+                curr+=prev1;
+            int two=(s[i-1]-'0')*10+(s[i]-'0');
+            if(two>=10 && two<=26)
+                curr+=prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
